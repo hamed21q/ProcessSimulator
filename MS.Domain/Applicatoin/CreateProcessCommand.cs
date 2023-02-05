f28bae1ed1c323b724f82db2eeca5f185dd13e69
hamed21q <hasanihamed220@gmail.com>
2023-02-05 06:55:17 -0800

    clean up

--- MS.Domain/Applicatoin/CreateProcessCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MS.Domain.Applicatoin
-{
-    public class CreateMemoryProcessCommand
-    {
-        public long SimulationId { get; set; }
-        public int ProcessSize { get; set; }
-    }
-}
